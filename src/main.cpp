8ddcbd3fd51ebcfaa45ac1cf3bf8228b2e924846
Mateusz Ciołkosz <matciolkosz@gmail.com>
2020-02-18 09:08:27 +0100

    Initial commit :)

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <main.hpp>
+
+#include <fstream>
+#include <iostream>
+
+// IN: input_filename/path
+// IN: output_filename/path
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
